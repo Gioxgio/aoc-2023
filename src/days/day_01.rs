05b84727582230111cb6b79a8640b55b11aa4b69
Giorgio Gagliardi <giorgio.gagliardi@klarna.com>
2023-11-30 16:57:56 +0100

    first commit

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -17,3 +17,43 @@ impl Day for Day01 {
         Ok(String::from(""))
     }
 }
+#[cfg(test)]
+mod test {
+
+    use std::error::Error;
+
+    use crate::days::day::Day;
+
+    use super::Day01;
+
+    static DAY: Day01 = Day01 {};
+
+    #[test]
+    fn test_part_1() -> Result<(), Box<dyn Error>> {
+        let input: Vec<String> = get_test_input();
+        let expected_result = "24000";
+
+        assert_eq!(DAY.part_1(&input)?, expected_result);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_2() -> Result<(), Box<dyn Error>> {
+        let input: Vec<String> = get_test_input();
+        let expected_result = "45000";
+
+        assert_eq!(DAY.part_2(&input)?, expected_result);
+
+        Ok(())
+    }
+
+    fn get_test_input() -> Vec<String> {
+        let input = "";
+
+        input
+            .lines()
+            .map(|s| s.trim().to_owned())
+            .collect::<Vec<String>>()
+    }
+}
