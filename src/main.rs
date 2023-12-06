77716a5bea394f156bb833b00836a2e1b2a0267a
Giorgio Gagliardi <giorgio.gagliardi@klarna.com>
2023-12-06 19:43:05 +0100

    Prepared day 07

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::error::Error;
 use aoc_2023::{
     days::{
         day::Day, day_01::Day01, day_02::Day02, day_03::Day03, day_04::Day04, day_05::Day05,
-        day_06::Day06,
+        day_06::Day06, day_07::Day07,
     },
     read_lines,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let days: Vec<&dyn Day> = vec![&Day01, &Day02, &Day03, &Day04, &Day05, &Day06];
+    let days: Vec<&dyn Day> = vec![&Day01, &Day02, &Day03, &Day04, &Day05, &Day06, &Day07];
 
     for day in days {
         let number = day.get_day();
